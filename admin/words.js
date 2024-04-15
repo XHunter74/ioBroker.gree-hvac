b777438ddbb32f449c51e8ea548d8b5e8dd84ef0
xhunter74 <xhunter74@gmail.com>
2024-04-15 22:16:23 +0300

    Updated transaltion.

--- admin/words.js
+++ admin/words.js
@@ -4,43 +4,43 @@
 'use strict';
 
 systemDictionary = {
-    'template adapter settings': {
-        'en': 'Adapter settings for template',
-        'de': 'Adaptereinstellungen für template',
-        'ru': 'Настройки адаптера для template',
-        'pt': 'Configurações do adaptador para template',
-        'nl': 'Adapterinstellingen voor template',
-        'fr': "Paramètres d'adaptateur pour template",
-        'it': "Impostazioni dell'adattatore per template",
-        'es': 'Ajustes del adaptador para template',
-        'pl': 'Ustawienia adaptera dla template',
-        'uk': 'Налаштування адаптера для template',
-        'zh-cn': 'template的适配器设置'
-    },
-    'option1': {
-        'en': 'option1',
-        'de': 'Option 1',
-        'ru': 'Опция 1',
-        'pt': 'Opção 1',
-        'nl': 'Optie 1',
-        'fr': 'Option 1',
-        'it': 'opzione 1',
-        'es': 'Opción 1',
-        'pl': 'opcja 1',
-        'uk': 'варіант1',
-        'zh-cn': '选项1'
-    },
-    'option2': {
-        'en': 'option2',
-        'de': 'Option 2',
-        'ru': 'вариант 2',
-        'pt': 'opção 2',
-        'nl': 'Optie 2',
-        'fr': 'Option 2',
-        'it': 'opzione 2',
-        'es': 'opcion 2',
-        'pl': 'Opcja 2',
-        'uk': 'варіант2',
-        'zh-cn': '选项2'
-    }
+    'gree-hvac adapter settings': {
+        "en": "Gree HVAC adapter settings",
+        "de": "Gree HVAC Adapter Einstellungen",
+        "ru": "Настройки адаптера Gree HVAC",
+        "pt": "Configurações do adaptador de HVAC da Gree",
+        "nl": "Gree HVAC-adapterinstellingen",
+        "fr": "Paramètres de l'adaptateur Gree CVC",
+        "it": "Impostazioni dell'adattatore Gree HVAC",
+        "es": "Ajustes de adaptador HVAC Gree",
+        "pl": "Ustawienia adaptera Gree HVAC",
+        "uk": "Параметри адаптера Gree HVAC",
+        "zh-cn": "Gree HVAC 适配器设置"
+      },
+    'devicelist': {
+        "en": "Device IPs, delimited by semicolon",
+        "de": "Geräte-IPs, begrenzt durch Semikolon",
+        "ru": "Устройства IP-адреса, разделенные полуколоном",
+        "pt": "IPs do dispositivo, delimitado por ponto e vírgula",
+        "nl": "Apparaat IP's, afgebakend door puntkomma",
+        "fr": "IP du périphérique, délimité par un point-virgule",
+        "it": "Dispositivi IP, delimitati da semicolon",
+        "es": "IPs de dispositivos, delimitados por semicolon",
+        "pl": "Urządzenie IP, wyznaczone przez średnik",
+        "uk": "IP пристроїв, розділені крапкою з зап'ятою",
+        "zh-cn": "设备 IP, 用分号分隔"
+      },
+    'pollInterval': {
+        "en": "Polling interval in milliseconds",
+        "de": "Polierintervall in Millisekunden",
+        "ru": "Интервал опроса в миллисекундах",
+        "pt": "Intervalo de polimento em milissegundos",
+        "nl": "Berekeninterval in milliseconden",
+        "fr": "Intervalle de scrutin en millisecondes",
+        "it": "Intervallo di inquinamento in millisecondi",
+        "es": "Intervalo de votación en milisegundos",
+        "pl": "Odstęp między badaniami w milisekundach",
+        "uk": "Інтервал опитування в мілісекундах",
+        "zh-cn": "投票间隔( 毫秒)"
+      }
 };
