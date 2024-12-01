e4df3baba0d6d6c9b19457c3a3bee239bb07c7ba
serhiy.krasovskyy <xhunter74@gmail.com>
2024-12-01 13:51:32 +0200

    Merge remote-tracking branch 'origin/dependabot/npm_and_yarn/chai-as-promised-8.0.1'
    # Conflicts:
    #	package-lock.json
    #	package.json

--- test/unit/encryptor.test.mjs
+++ test/unit/encryptor.test.mjs
@@ -1,7 +1,8 @@
-const { expect } = require('chai');
+import { expect } from 'chai';
+import { encryptV1, decryptV1, encryptV2, decryptV2, defaultKeyGCM } from '../../lib/encryptor.js';
 
 describe('Encrypt and Decrypt data using AES-128-ECB', () => {
-    const { encryptV1, decryptV1, defaultKeyGCM } = require('../../lib/encryptor');
+    // const { encryptV1, decryptV1, defaultKeyGCM } = require('../../lib/encryptor');
     const expectedEncryptedData = 'LP24Ek0OaYogxs3iQLjL4CVWHqmPoWzCNdstE6VtRN5YT1PXPBDouOPiWHaG4G8lz22fUZtUbkblUYF5BzlKDV9xeQzMlsaP4RKBtrnsDrPgJXA+/2/qaGLK2Z7N/04mLjfzIBDIkDxmTYsl4aiW1+80HbfgK3MNVtBb+mRtQFwvWGtrb35TAKQLbuUeHhrjkywWYwaOwxo65kKReDaAYU+t/W6ao6KLeI6AQs8sVr6ZbdEmdMsvWjDwsWTPw4iozbbXHI8ghbocDlA7fUL03g==';
 
     const data = {
@@ -32,7 +33,7 @@ describe('Encrypt and Decrypt data using AES-128-ECB', () => {
 });
 
 describe('Encrypt and Decrypt data using AES-128-GCM', () => {
-    const { encryptV2, decryptV2, defaultKeyGCM } = require('../../lib/encryptor');
+    // const { encryptV2, decryptV2, defaultKeyGCM } = require('../../lib/encryptor');
     const expectedEncryptedData = 'JtoKliwtrZWlpNCVOSARFZVjvdMQgUTwMFEwCLvahOTTdG5N10M5OI3w9aCGCJffjfuyCITofrMT4JbII6+A1+2Qyk7gfwk5dZR2EayhdZgEoOSGGofp1NG7sM5o7a8eFq+2oChWDqTBGSbpwgXi7D3lH3MHl69lO9CHVqNeh4BiujYkVwS8ZeC2r1srS6O8i+ryGhedh+q+4CvnkIN+M2+73Ds6jUrdpcHFeTxudL18EfiO0s52M78lga9J8AtcIaaDqvM0';
     const expectedEncryptedTag = '596Gsbo+odGTNGIgr+YVFQ==';
 
